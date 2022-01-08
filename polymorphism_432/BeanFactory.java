39a8958aa13935b9bb2cac90d5682a10b174ba21
Seona Yang <n2nnn2n@gmail.com>
2022-01-08 10:27:50 +0900

    first commit

---
+++ polymorphism_432/BeanFactory.java
@@ -0,0 +1,15 @@
+package polymorphism_432;
+
+public class BeanFactory {
+	public Object getBean(String beanName) {
+		if (beanName.equals("samsung")) {
+			return new SamsungTV();
+		} else if (beanName.equals("lg")) {
+			return new LgTV();
+		}
+		return null;
+	}
+	
+
+}
+
---
+++ polymorphism_432/Speaker.java
@@ -0,0 +1,6 @@
+package polymorphism_432;
+
+public interface Speaker {
+	void volumeUp();
+	void volumeDown();
+}
